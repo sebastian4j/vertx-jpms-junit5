6a03b2aed8ec967167ef88ed302e1346e14020cb
Sebastian Avila A <sebastian4j@gmail.com>
2019-04-16 00:42:59 -0400

    permite construir el JRE necesario para ejecutar la app

---
+++ link.sh
@@ -0,0 +1,9 @@
+echo "buscando modulos"
+cd target
+JAVA_BASE_MODS=$(find . -name "*.jar" -exec jdeps --module-path libs:*.jar -s {} \; | sed -En "s/.* -> (java.*)/ \1;/p" | sort | uniq | grep -v "java.annotation" | tr -s ';\n ' ',')
+echo "construir jre"
+${JAVA_HOME}/bin/jlink --module-path "${JAVA_HOME}/jmods:libs:*.jar" \
+    --add-modules java.management,jdk.unsupported"${JAVA_BASE_MODS}" \
+    --strip-debug --compress 2 --no-man-pages \
+    --no-header-files --output jre
+echo "ok"
